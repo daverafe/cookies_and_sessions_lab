efe55ef6d35433e6a6166e4715f7464e1d26ca6c
David Rafe <david.rafe@gmail.com>
2021-04-19 16:27:34 -0400

    commit tree 63ac38f5a899c20c383d162b4e772a3f64f3e63c

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,13 +1,11 @@
 class ProductsController < ApplicationController
 
     def index
-      @cart = cart 
     end
 
     def create
-        @cart = cart 
-        @cart << params[:product]
-        redirect_to products_path
+        cart << params[:product]
+        render :index 
     end
 
 end
